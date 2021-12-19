ab25c55ca7b6c2323de28700a67c9b4fc528f8af
Harry-KNIGHT <elliotharryk@gmail.com>
2021-12-19 13:16:12 +0100

    5th day till default parameters

--- Pages/Day5.xcplaygroundpage/Contents.swift
+++ Pages/Day5.xcplaygroundpage/Contents.swift
@@ -1,7 +1,86 @@
 //: [Previous](@previous)
 
 import Foundation
+import CoreFoundation
 
-var greeting = "Hello, playground"
+// 1. Writing functions
 
-//: [Next](@next)
+func helloWorld() {
+    let message = "HelloWorld"
+    print(message)
+}
+helloWorld()
+
+// 2. Accepting parameters
+
+func square(number: Int) {
+    print(number * number)
+}
+square(number: 7)
+
+// 3. Returning value
+
+func squareTwo(number: Int) -> Int {
+    return number * number
+}
+let result = squareTwo(number: 8)
+print(result)
+
+// Array
+func getUser() -> [String] {
+    ["Taylor", "Swift"]
+}
+
+let user = getUser()
+print(user[0])
+
+/* Dictionnary
+func getUserTwo() -> [String: String] {
+    ["first": "Taylor", "last": "Swift"]
+}
+let userTwo = getUserTwo()
+print(userTwo["last"])
+*/
+
+// 4. Parameter labels
+
+func sayHello(to name: String) {
+    print("Hello \(name)")
+}
+/*
+ The parameter is called to name, which means externally it’s called to, but internally it’s called name. This gives variables
+ a sensible name inside the function, but means calling the function reads naturally:
+ */
+sayHello(to: "Elliot")
+
+// 5. Omitting parameter labels
+
+func greet(_ person: String) {
+    print("Hello, \(person)")
+}
+greet("Arthur")
+// greet(person: "zizi") This would make an error due to the _
+/* You can get this same behavior in your own functions by using an underscore, _, for your external parameter name.
+You can now call greet() without having to use the person parameter name */
+
+
+// 6. Default parameters
+
+func greets(_ person: String, nicely: Bool = true) {
+    if nicely == nicely {
+        print("Hi \(person) how are you ?")
+    }else {
+        ("Oh no it's \(person) again ...!")
+    }
+}
+greets("Bouba", nicely: false)
+
+// 7. Variadic functions
+
+
+// 8. Writing throwing functions
+
+// 9. Running throwing functions
+
+// 10. Inout parameters
+
