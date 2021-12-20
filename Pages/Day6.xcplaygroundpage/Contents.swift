00c63d200170aaddf86c2dbbca429677558c98cb
Harry-KNIGHT <elliotharryk@gmail.com>
2021-12-20 12:03:21 +0100

    Returning value from a closure

--- Pages/Day6.xcplaygroundpage/Contents.swift
+++ Pages/Day6.xcplaygroundpage/Contents.swift
@@ -1,7 +1,32 @@
 //: [Previous](@previous)
 
 import Foundation
+import UIKit
 
-var greeting = "Hello, playground"
 
-//: [Next](@next)
+
+// 1. Creating basic closures —————————————————————————————————————————————————————————————————————————————————————————————————
+let driving = {
+    print("Baby you can drive my car")
+}
+driving()
+
+// 2.Accepting parameters in a closure ————————————————————————————————————————————————————————————————————————————————————————
+
+let drive = { (city: String) in
+    print("I'm going to \(city) in my car !")
+}
+drive("Paris")
+// 3. Returning values from a closure —————————————————————————————————————————————————————————————————————————————————————————
+
+let drivingWithReturn = { (place: String) -> String in
+   return "I am going to \(place)"
+}
+
+let message = drivingWithReturn("London")
+print(message)
+// 4. Closures as parameters ——————————————————————————————————————————————————————————————————————————————————————————————————
+
+// 5.Trailing closure syntax ——————————————————————————————————————————————————————————————————————————————————————————————————
+
+// 6.       —————————————————————————————————————————————————————————————————————————————————————————————————
