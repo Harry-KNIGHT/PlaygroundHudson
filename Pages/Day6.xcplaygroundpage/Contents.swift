ca6293ca4e3e51bf29aefcd8cbe752934c4983bc
Harry-KNIGHT <elliotharryk@gmail.com>
2021-12-22 17:32:28 +0100

    Fifth day

--- Pages/Day6.xcplaygroundpage/Contents.swift
+++ Pages/Day6.xcplaygroundpage/Contents.swift
@@ -25,8 +25,25 @@ let drivingWithReturn = { (place: String) -> String in
 
 let message = drivingWithReturn("London")
 print(message)
-// 4. Closures as parameters ——————————————————————————————————————————————————————————————————————————————————————————————————
 
-// 5.Trailing closure syntax ——————————————————————————————————————————————————————————————————————————————————————————————————
+// 4. Closures as parameters 5.Trailing closure syntax ————————————————————————————————————————————————————————————————————————
 
-// 6.       —————————————————————————————————————————————————————————————————————————————————————————————————
+/*
+Because closures can be used just like strings and integers, you can pass them into functions.
+ 
+If we wanted to pass that closure into a function so it can be run inside that function, we would specify the parameter type
+as () -> Void. That means “accepts no parameters, and returns Void” – Swift’s way of saying “nothing”.
+
+So, we can write a travel() function that accepts different kinds of traveling actions, and prints a message before and after:
+ */
+
+func travel(action: () -> Void) {
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived!")
+}
+travel(action: driving)
+
+travel {
+    print("I'm driving my car")
+}
