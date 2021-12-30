1a131d744bf6874ea750204252268d0ad6abac2f
Harry-KNIGHT <elliotharryk@gmail.com>
2021-12-30 16:06:12 +0100

    Day 13

---
+++ Pages/Day 13.xcplaygroundpage/Contents.swift
@@ -0,0 +1,67 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
+// Review what we seen last 12 days.
+
+
+// 1. Variables, constants
+
+var name = "Elliot"
+name = "Arthur"
+
+var firstName = "Jesus"
+firstName = "Jesus Espejo"
+
+let counter = 10
+//conter -= 1  It cant be made bc it's a constant
+
+// 2. Types of data
+
+var bike: String = "Decathlon"
+bike = "Go Sport"
+
+var nothingInBrain: Bool
+nothingInBrain = true
+
+var floatNumber: Float = 192384.4455
+
+var doubleNumber: Double = 192384.4455
+
+var integer: Int = 1255
+
+
+// 3. Operator
+
+var a = 10
+a = a + 1
+a = a - 1
+a = a * 10
+
+a += 1
+a -= 1
+a *= 10
+a = 10
+
+var b = 10
+
+var c = a + b
+
+c > 10
+c > 100
+c >= 20
+
+var name1 = "Tobey Maguire"
+var name2 = "TObey MAguire"
+var both = name1 + " and " + name2
+
+name1 == name2
+name1 != "Tobey Maguire"
+
+
+
+
+
